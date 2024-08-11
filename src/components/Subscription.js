a476421c1cb7aadf643dffe6b3252a2e502522f9
anup27 <anup@famepilot.com>
2024-08-11 22:39:27 +0530

    props along children

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,15 +1,18 @@
 import "./Subscription.css";
+import SubscriptionDate from "./SubscriptionDate";
+import Container from "./Container";
 
 function Subscription(props) {
-  const month = props.date.toLocaleString("en-US",{month:'long'});
-  const day   = props.date.toLocaleString("en-US",{day:'2-digit'});
-  const year  = props.date.getFullYear();
   return (
-    <div className="subscription">
-      <div>{day} - {month} - {year}</div>
+    <Container className="subscription">
+      <SubscriptionDate
+        date={props.date}
+        title={props.title}
+        amount={props.amount}
+      ></SubscriptionDate>
       <h2 className="subscription_title">{props.title}</h2>
       <div className="subscription_price">{props.amount}</div>
-    </div>
+    </Container>
   );
 }
 
