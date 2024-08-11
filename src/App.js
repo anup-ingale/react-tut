63c6675cb4180977419b8fcbac46bd7d95dd857c
anup27 <anup@famepilot.com>
2024-08-11 22:56:50 +0530

    used arrow function and manage folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Subscription from './components/Subscription'
-import Container from './components/Container';
+import Subscription from './components/Subscription/Subscription'
+import Container from './components/templates/Container';
 import React from 'react';
 
 
 
-function App() {
+const App =()=> {
   let subscriptions = [
     {
       id:"1",
@@ -28,12 +28,12 @@ function App() {
   ]
 
   return (
-    React.createElement('div',{},React.createElement('h2',{},"Let's Start !! "))
-  //   <Container>
-  //     <Subscription date={subscriptions[0].date} title={subscriptions[0].title} amount={subscriptions[0].amount} />
-  //     <Subscription date={subscriptions[1].date} title={subscriptions[1].title} amount={subscriptions[1].amount} />
-  //     <Subscription date={subscriptions[2].date} title={subscriptions[2].title} amount={subscriptions[2].amount} />
-  //  </Container>
+    // React.createElement('div',{},React.createElement('h2',{},"Let's Start !! "))
+    <Container>
+      <Subscription date={subscriptions[0].date} title={subscriptions[0].title} amount={subscriptions[0].amount} />
+      <Subscription date={subscriptions[1].date} title={subscriptions[1].title} amount={subscriptions[1].amount} />
+      <Subscription date={subscriptions[2].date} title={subscriptions[2].title} amount={subscriptions[2].amount} />
+   </Container>
   )
 }
 
